ad80d3579af0916869f6568405f0552287c955e8
Subhajit Sahu <wolfram77@gmail.com>
2016-09-24 23:33:24 +0530

    program: dev & test :seedling:

--- src/Program.cs
+++ src/Program.cs
@@ -2,7 +2,17 @@
 
 namespace orez.oclear {
     class Program {
+
+        // static method
+        /// <summary>
+        /// Pitying yourself has terrible connotations, but it
+        /// belongs to what it means to be a friend to oneself.
+        /// : The School of Life
+        /// </summary>
+        /// <param name="args">Input arguments.</param>
         static void Main(string[] args) {
+            Console.WindowLeft = 0;
+            Console.WindowTop = Console.CursorTop;
         }
     }
 }
