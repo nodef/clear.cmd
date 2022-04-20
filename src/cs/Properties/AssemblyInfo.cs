abd39b3673d3bbf3f51f7fc9fa1322808b0fec49
Subhajit Sahu <wolfram77@gmail.com>
2022-04-20 23:11:16 +0530

    :turtle: pack csharp into src/cs/

--- src/cs/Properties/AssemblyInfo.cs
+++ src/cs/Properties/AssemblyInfo.cs
@@ -2,20 +2,20 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("extra-clear")]
+[assembly: AssemblyTitle("clear.cmd")]
 [assembly: AssemblyDescription("Clear screen like Unix in Windows Console.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Merferry")]
-[assembly: AssemblyProduct("extra-clear")]
-[assembly: AssemblyCopyright("Copyright (c) 2016 Merferry")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("clear.cmd")]
+[assembly: AssemblyCopyright("Copyright (c) 2016-22 Subhajit Sahu")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(true)]
 
@@ -25,11 +25,11 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
