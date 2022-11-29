ebdab098c98c74613ef0a04d18b62e23d822a3d7
Subhajit Sahu <wolfram77@gmail.com>
2022-11-29 19:46:01 +0530

    :bug: update build setup

--- build.js
+++ build.js
@@ -1,16 +1,51 @@
-const cp = require('child_process');
+const cp    = require('child_process');
+const build = require('extra-build');
 
+const owner     = 'nodef';
 const CLEAR_URL = 'https://github.com/nodef/clear.cmd/releases/download/1.0.0/clear.exe';
 
 
-function fetchSuper(url) {
+// Publish a root package to NPM, GitHub.
+function publishRootPackage(ds, ver, typ) {
+  var _package = build.readDocument('package.json');
+  var _readme  = build.readDocument('README.md');
+  var m  = build.readMetadata('.');
+  m.version  = ver;
+  build.writeMetadata('.', m);
+  build.publish('.');
+  try { build.publishGithub('.', owner); }
+  catch {}
+  build.writeDocument(_package);
+  build.writeDocument(_readme);
+}
+
+
+// Download executable file (needs wget).
+function fetchRelease(url) {
   cp.execSync(`wget -nv ${url}`);
   cp.execSync(`mv clear.exe main.exe`);
   cp.execSync(`chmod +x main.exe`);
 }
 
 
-function main() {
-  fetchSuper(CLEAR_URL);
+// Publish root packages to NPM, GitHub.
+function publishRootPackages(ds, ver) {
+  fetchRelease(CLEAR_URL);
+  publishRootPackage(ds, ver, '');
 }
-main();
+
+
+// Pushish root, sub packages to NPM, GitHub.
+function publishPackages(ds) {
+  var m   = build.readMetadata('.');
+  var ver = build.nextUnpublishedVersion(m.name, m.version);
+  publishRootPackages(ds, ver);
+}
+
+
+// Finally.
+function main(a) {
+  if (a[2]==='publish-packages') publishPackages([]);
+  else bundleScript([]);
+}
+main(process.argv);
